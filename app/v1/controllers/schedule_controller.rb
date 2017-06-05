55736bfa549626f98f61824a9bf539166ee245ff
TPei <t.s.peikert@gmail.com>
2017-06-05 12:38:09 +0200

    schedule_controller hash syntax fix

--- app/v1/controllers/schedule_controller.rb
+++ app/v1/controllers/schedule_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'dotenv'
+require 'json'
 require './app/workers/infection_schedule_worker'
 
 module Api
@@ -9,11 +10,11 @@ module Api
       class ScheduleController < Sinatra::Base
         post '/schedule' do
           body = JSON.load(request.body.read)
-          unless key = body.game_key
+          unless key = body['game_key']
             halt 422 # unprocessable entity
           end
 
-          time = body.time || ENV['DEFAULT_INFECTION_TIME']
+          time = body['time'] || ENV['DEFAULT_INFECTION_TIME']
           InfectionScheduleWorker.perform_async(key, time)
         end
       end
