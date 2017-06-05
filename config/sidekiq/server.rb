a80f8a4a59895005095454876e4617ac6c7a6de3
TPei <t.s.peikert@gmail.com>
2017-06-05 12:29:35 +0200

    syntax fix

--- config/sidekiq/server.rb
+++ config/sidekiq/server.rb
@@ -3,7 +3,7 @@
 require 'app/middleware/sidekiq_logger'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL' || 'redis://redis:6379' }
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://redis:6379' }
 
   config.server_middleware do |chain|
     chain.add Middleware::SidekiqLogger
